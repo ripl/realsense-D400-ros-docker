051cb9800bf8603accb3f81af073a77d733b0bcb
Chip Schaff <cbschaff@ttic.edu>
2023-03-16 15:05:00 -0500

    add camera configuration json files (obtained from realsense-viewer)

--- launchers/bash.sh
+++ launchers/bash.sh
@@ -10,4 +10,4 @@ echo "This is an empty launch script. Update it to launch your application."
 
 # ----------------------------------------------------------------------------
 # YOUR CODE ABOVE THIS LINE
-roslaunch realsense2_camera rs_camera.launch
+bash
--- launchers/d415.sh
+++ launchers/d415.sh
@@ -10,4 +10,4 @@ echo "This is an empty launch script. Update it to launch your application."
 
 # ----------------------------------------------------------------------------
 # YOUR CODE ABOVE THIS LINE
-roslaunch realsense2_camera rs_camera.launch
+roslaunch realsense2_camera rs_camera.launch json_file_path:=/code/src/realsense-D400-ros-docker/assets/cam-d415.json
